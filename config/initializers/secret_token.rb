3733dc31bb0bfb7ce1bc8f84d5ccb296a59b488d
Mike Skwarcan <mikeskwarcan@Mike-Skwarcans-iMac.local>
2011-03-17 11:02:44 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Design4tomorrow::Application.config.secret_token = '5dd59342597ee91d33369219fc468f0fc1758bf44c00f7f62c3e05ea4ae49388adf0614c24061388a0af7bc433bf3605aa23af4f91ab82829628dc76b835c930'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Design4tomorrow::Application.config.session_store :cookie_store, :key => '_design4tomorrow_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Design4tomorrow::Application.config.session_store :active_record_store
